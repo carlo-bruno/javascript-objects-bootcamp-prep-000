8699a4ae558b8f5d2f3b5e0a8094f4bcf2679c6f
Carlo Bruno <carlofbruno@gmail.com>
2018-11-08 05:58:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {beatles: "Hey Jude"};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
